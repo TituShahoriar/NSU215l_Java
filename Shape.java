10ff098dab23df16917c09476667b560cd85ff4f
Md Fahim Shahoriar Titu <fahimshahoriar66@gmail.com>
2023-06-12 15:18:37 +0600

    Add files via upload

--- Shape.java
+++ Shape.java
@@ -1,51 +1,19 @@
-abstract class Shape {
-    private String color;
+public class Shape {
+    private String name;
 
-    public Shape(String color) {
-        this.color = color;
+    public Shape(String name) {
+        this.name = name;
     }
 
-    public abstract double getArea();
-
-    public String toString() {
-        return "Color: " + color;
-    }
-}
-
-class Rectangle extends Shape {
-    private int length;
-    private int width;
-
-    public Rectangle(String color, int length, int width) {
-        super(color);
-        this.length = length;
-        this.width = width;
+    public String getName() {
+        return name;
     }
 
-    public double getArea() {
-        return length * width;
-    }
-
-    public String toString() {
-        return super.toString() + ", Length: " + length + ", Width: " + width + ", Area: " + getArea();
+    public void setName(String name) {
+        this.name = name;
     }
 }
 
-class Triangle extends Shape {
-    private int base;
-    private int height;
 
-    public Triangle(String color, int base, int height) {
-        super(color);
-        this.base = base;
-        this.height = height;
-    }
 
-    public double getArea() {
-        return 0.5 * base * height;
-    }
 
-    public String toString() {
-        return super.toString() + ", Base: " + base + ", Height: " + height + ", Area: " + getArea();
-    }
-}
