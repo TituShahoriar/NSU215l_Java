3d2a1e10594c5e2e10f660c6d3047295c524e507
Md Fahim Shahoriar Titu <fahimshahoriar66@gmail.com>
2023-06-12 15:21:10 +0600

    Add files via upload

---
+++ FullTimeEmployee.java
@@ -0,0 +1,5 @@
+public class FullTimeEmployee extends Employee {
+    public FullTimeEmployee(String department, String designation, double basic, double allowance) {
+        super(department, designation, basic, allowance);
+    }
+}
