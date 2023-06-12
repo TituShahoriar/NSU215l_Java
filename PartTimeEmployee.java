f95af1d9d99e9534172df74d41922c9c61052f3a
Md Fahim Shahoriar Titu <fahimshahoriar66@gmail.com>
2023-06-12 15:20:00 +0600

    Add files via upload

---
+++ PartTimeEmployee.java
@@ -0,0 +1,5 @@
+public class PartTimeEmployee extends Employee {
+    public PartTimeEmployee(String department, String designation, double basic, double allowance) {
+        super(department, designation, basic, allowance);
+    }
+}
