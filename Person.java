66ca18b928954d3f9638d5b0184746de309bb60d
Md Fahim Shahoriar Titu <fahimshahoriar66@gmail.com>
2023-06-12 15:19:41 +0600

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    private String department;
+    private String designation;
+
+    public Person(String department, String designation) {
+        this.department = department;
+        this.designation = designation;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+}
